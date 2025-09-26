52a0fb9ea6678060c2017b418ed50f1d5ebb2502
ZionPrado <raiprado04@gmail.com>
2025-09-26 01:04:02 +0000

    Final

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -21,7 +21,7 @@ int main() {
   
 
   printf("Digite sua Cidade: \n");
-  fgets(Cidade, 50, stdin);// Entrar com Nome Composto Ex. Rio de Janeiro!
+  fgets(Cidade, 50, stdin); // Entrar com Nome Composto Ex. Rio de Janeiro!
 
 
 
@@ -55,6 +55,55 @@ int main() {
   printf(" Área: %f\n PIB: %f\n Pontos Turísticos: %d\n", Area, PIB, pontos);
   printf(" Densidade populacional: %.2f\n", densidade);
   printf(" PIB per Capita: %.2f\n", percapita);
+
+  float super;
+   
+  super = populacao + pontos + PIB + Area + percapita + 1/densidade;
+
+  printf(" Super poder: %.2f\n\n\n\n\n", super);    
+
+
+
+  char Cidade2 [60];
+  char estado2;
+  char Codigo2 [7];
+  int populacao2;
+  int pontos2;
+  float PIB2;
+  float Area2;
+  
+  printf("Digite sua Cidade: \n");
+  scanf("%s", &Cidade2);// Entrar com Nome Composto Ex. Rio de Janeiro!
+
+  printf("Digite a letra do Estado:\n");
+  scanf("%s", &estado2)
+
+  printf("Digite o Codigo coma Letra do seu Estado de 01 a 04 (Ex:A01):\n");
+  scanf("%s", &Codigo2);
+
+  printf("Digite a População: \n");
+  scanf("%d", &populacao2);
+
+  printf("Digite a Área em Km²: \n");
+  scanf("%f", &Area2);
+
+  printf("Digite o PIB: \n");
+  scanf("%f", &PIB2);
+
+  printf("Digite a quantidade Pontos Turísticos: \n");
+  scanf("%d", &pontos2);
+
+   float densidade2;
+  float percapita2;
+
+  densidade2 = ( float ) populacao2 / Area2;
+  percapita2 = ( float ) PIB2 / populacao2;
+
+  printf(" Carta 2:\n");
+  printf(" Estado: %c\n Código: %s\n Cidade: %s População: %d\n", estado2, Codigo2, Cidade2, populacao2);
+  printf(" Área: %f\n PIB: %f\n Pontos Turísticos: %d\n", Area2, PIB2, pontos2);
+  printf(" Densidade populacional: %.2f\n", densidade2);
+  printf(" PIB per Capita: %.2f\n", percapita2);
 
 
 
